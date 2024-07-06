152c37ac6aecf09e037a59af472ee61d0aee8274
Necktwi Ozfguah <Necktwi@FerryFair.com>
2024-07-06 13:19:43 +0530

    cgal

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -1,8 +1,25 @@
+/*                              Column index
+12345678902134567890312456789041235678905123467890612345789071234568908123456790
+ */
 // Include standard headers
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <cmath>
+#include <LASlib/lasreader.hpp>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+//#include <CGAL/Cartesian.h>
+#include <CGAL/Projection_traits_xy_3.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+#include <CGAL/boost/graph/graph_traits_Delaunay_triangulation_2.h>
+#include <CGAL/boost/graph/copy_face_graph.h>
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+#include <CGAL/point_generators_2.h>
+#include <CGAL/Triangulation_vertex_base_with_info_2.h>
+#include <CGAL/Triangulation_face_base_with_info_2.h>
+#include <unordered_map>
 
 // Include GLEW
 #include <GL/glew.h>
@@ -18,6 +35,20 @@ float height=600.0f;
 #include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
 using namespace std;
+using namespace CGAL;
+typedef Exact_predicates_inexact_constructions_kernel            K;
+//typedef Cartesian<float>            K;
+typedef Projection_traits_xy_3<K>                                PrjTrts;
+typedef Triangulation_vertex_base_with_info_2<unsigned, PrjTrts> Vb2;
+typedef Triangulation_face_base_with_info_2<int, PrjTrts>        Fb2;
+typedef K::Point_3                                               Point3;
+//typedef K::Vector_3                                              Vec3;
+typedef Surface_mesh<Point3>                                     Mesh;
+typedef Triangulation_data_structure_2<Vb2>                      T2ds;
+//typedef Triangulation_data_structure_2<Vb2, Fb2>                 T2ds;
+typedef Delaunay_triangulation_2<PrjTrts, T2ds>                  TIN;
+//typedef Delaunay_triangulation_2<PrjTrts>                        TIN;
+
 // Initial position : on +Z
 // Initial horizontal angle : toward -Z
 float horizontalAngle = 3.14f;
@@ -106,6 +137,8 @@ void NormalSmooth (
    }
 }
 
+mat4 rotate (mat4 r, vec3 v);
+
 int main( void ) {
    if( !glfwInit() ) {
       fprintf( stderr, "Failed to initialize GLFW\n" );
@@ -120,8 +153,8 @@ int main( void ) {
    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
    glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
    
-   window = glfwCreateWindow
-   ((int)width, (int)height, "Playground", NULL, NULL);
+   window = glfwCreateWindow(
+      (int)::width, (int)height, "Playground", NULL, NULL);
    if ( window == NULL ) {
       fprintf ( stderr, "Failed to open GLFW window. If you have an Intel GPU,"
       "they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
@@ -145,10 +178,10 @@ int main( void ) {
    glfwSetInputMode (window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
    // Set the mouse at the center of the screen
    glfwPollEvents ();
-   glfwSetCursorPos (window, width/2, height/2);
+   glfwSetCursorPos (window, ::width/2, height/2);
    
    // Dark blue background
-   glClearColor (0.0f, 0.0f, 0.4f, 0.0f);
+   glClearColor (0.0f, 0.0f, 0.0f, 0.0f);
    
    // Enable depth test
    glEnable (GL_DEPTH_TEST);
@@ -159,10 +192,10 @@ int main( void ) {
    glGenVertexArrays (1, &VertexArrayID);
    glBindVertexArray (VertexArrayID);
    
-   GLuint programID = LoadShaders ( "vertexplayer.vertexshader",
-                                    "fragmentplayer.fragmentshader" );
+   GLuint programID = LoadShaders ( "vertexshader.glsl",
+                                    "fragmentshader.glsl" );
    GLuint MatrixID = glGetUniformLocation ( programID, "MVP");
-   
+   GLuint zHalfMaxID = glGetUniformLocation ( programID, "zHalfMax");
    
    static const GLfloat g_vertex_buffer_data [] = {
       -0.5f, -1.0f, -1.0f,
@@ -234,6 +267,129 @@ int main( void ) {
        0.0f, 0.0f,-1.0f,
       -1.0f, 0.0f, 0.0f
    };
+   static const GLfloat gfTriangleBufferData [] = {
+       0.0f,  1.0f, 0.0f,
+      -1.0f, -1.0f, 0.0f,
+       1.0f, -1.0f, 0.0f
+   };
+   static const unsigned gfTriangleElementData [] = {
+      0, 1, 2
+   };
+   static const GLfloat gfTriColorBufferData [] = {
+      1.0f, 0.0f, 0.0f,
+      1.0f, 0.0f, 0.0f,
+      1.0f, 0.0f, 0.0f
+   };
+   unordered_map<Point3, unsigned> mapP3;
+   vector<vec3> vecV3;
+   vector<unsigned> vecElms;
+   LASreadOpener lasreadopener;
+   lasreadopener.set_file_name("/home/Necktwi/workspace/displaz/SU785700.las");
+   LASreader* lasreader = lasreadopener.open();
+   LASpoint& pt = lasreader->point;
+   size_t i=0;
+   vec3 centroid;
+   vec3 bboxMax;
+   vec3 bboxMin;
+   while (lasreader->read_point()) {
+      if ((pt.classification | (pt.synthetic_flag << 5) |
+          (pt.keypoint_flag << 6) | (pt.withheld_flag << 7))!=2) {
+         Point3 p3(pt.get_x(), pt.get_y(), pt.get_z());
+         vec3 v3(pt.get_x(), pt.get_y(), pt.get_z());
+         if (!i)
+            centroid=v3;
+         else {
+            centroid+=v3;
+            centroid/=2;
+         }
+         vecV3.push_back(v3);
+         mapP3[p3]=i++;
+         if (i>=1000)
+            break;
+      }
+   }
+   printf("mapP3 size: %ld\n"
+          "sizeof(Point3): %u\n"
+          "sizeof(vec3): %u\n"
+          "centroid: %f,%f,%f\n", mapP3.size(), sizeof(Point3), sizeof(vec3),
+          centroid.x, centroid.y, centroid.z);
+   TIN dsm(mapP3.begin(), mapP3.end());
+   Mesh sm;
+   CGAL::copy_face_graph(dsm, sm);
+   for (Mesh::Face_index fi : sm.faces()) {
+      for (Mesh::Vertex_index vi:
+              vertices_around_face(sm.halfedge(fi), sm)) {
+         vecElms.push_back(mapP3[sm.point(vi)]);
+      }
+   }
+   for (unsigned i=0; i<vecV3.size(); ++i) {
+      vecV3[i]-=centroid;
+      bboxMax.x=bboxMax.x>vecV3[i].x?bboxMax.x:vecV3[i].x;
+      bboxMin.x=bboxMin.x<vecV3[i].x?bboxMin.x:vecV3[i].x;
+      bboxMax.y=bboxMax.x>vecV3[i].y?bboxMax.y:vecV3[i].y;
+      bboxMin.y=bboxMin.x<vecV3[i].y?bboxMin.y:vecV3[i].y;
+      bboxMax.z=bboxMax.z>vecV3[i].z?bboxMax.z:vecV3[i].z;
+      bboxMin.z=bboxMin.z<vecV3[i].z?bboxMin.z:vecV3[i].z;
+   }
+   float zHalfMax = bboxMax.z>-bboxMin.z?bboxMax.z:-bboxMin.z;
+   zHalfMax/=2;
+   printf("vecElms.size: %u\n"
+          "zHalfMax: %f\n"
+          "bboxMax: %f,%f,%f\n"
+          "bboxMin: %f,%f,%f\n", vecElms.size(), zHalfMax,
+          bboxMax.x,bboxMax.y,bboxMax.z, bboxMin.x, bboxMin.y, bboxMin.z);
+   GLuint pcVertexBuffer;
+   glGenBuffers(1, &pcVertexBuffer);
+   glBindBuffer(GL_ARRAY_BUFFER, pcVertexBuffer);
+   // glBufferData(GL_ARRAY_BUFFER, sizeof(vec3)*vecV3.size(),
+   //               &vecV3[0], GL_STATIC_DRAW);
+   glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*9, &gfTriangleBufferData[0],
+                GL_STATIC_DRAW);
+   GLuint pcElmBuffer;
+   glGenBuffers(1, &pcElmBuffer);
+   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, pcElmBuffer);
+   // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned)*vecElms.size(),
+   //              &vecElms[0], GL_STATIC_DRAW);
+   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned)*3,
+               &gfTriangleElementData[0], GL_STATIC_DRAW);
+   glUniformMatrix4fv(zHalfMaxID, 1, GL_FALSE, &zHalfMax);
+   
+   glUseProgram (programID);
+   glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+      
+   do {
+      glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      computeMatricesFromInputs();
+      mat4 scale (1.0f);
+      mat4 rot (1.0f);
+      mat4 trans (1.0f);
+      rot = rotate (rot, vec3(0.0f));
+      scale = glm::scale (scale, vec3(1000));
+      trans = translate (mat4(1.0f), vec3(0.0f));
+      
+      mat4 model (1.0f);
+      model = trans * scale * rot;
+      mat4 MVP = ProjectionMatrix * ViewMatrix * model;
+      glUniformMatrix4fv (MatrixID, 1, GL_FALSE, &MVP [0][0]);
+      glEnableVertexAttribArray (0);
+      glBindBuffer (GL_ARRAY_BUFFER, pcVertexBuffer);
+      glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, pcElmBuffer);
+      glDrawElements(
+         GL_TRIANGLES,      // mode
+//         vecElms.size(),    // count
+         3,
+         GL_UNSIGNED_INT,   // type
+         (void*)0           // element array buffer offset
+      );
+      glDisableVertexAttribArray(0);
+      glfwSwapBuffers (window);
+      glfwPollEvents ();
+   } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+            glfwWindowShouldClose(window) == 0);
+   glDeleteBuffers (1, &pcElmBuffer);
+   glDeleteBuffers (1, &pcVertexBuffer);
+   return 0;
    static const unsigned int uiNumSphericalVertices = 3*8*4*4*4*4*4;
    static const unsigned int uiNumAstVertices = 3*8*4*4*4;
    GLfloat gfSphereVertexBufferData [3*uiNumSphericalVertices];
@@ -245,7 +401,7 @@ int main( void ) {
    // One color for each vertex. They were generated randomly.
    float r, g, b;
    GLfloat earthColBufData [3*uiNumSphericalVertices];
-   for (int v = 0; v < uiNumSphericalVertices; ++v){
+   for (int v = 0; v < uiNumSphericalVertices; ++v) {
       //193
       if ((v>767 && v<1536) || (v>3071 && v<3840) ||
          (v>13055 && v<13824) || (v>15359 && v<16128)
@@ -289,16 +445,6 @@ int main( void ) {
       b = b >= 1.0f ? (b - 1.0f) : b;
       astColBufData [3 * v + 2] = r;
    }
-   static const GLfloat gfTriangleBufferData [] = {
-       0.0f,  1.0f, 0.0f,
-      -1.0f, -1.0f, 0.0f,
-       1.0f, -1.0f, 0.0f
-   };
-   static const GLfloat gfTriColorBufferData [] = {
-      1.0f, 0.0f, 0.0f,
-      1.0f, 0.0f, 0.0f,
-      1.0f, 0.0f, 0.0f
-   };
    static const GLfloat gfCubeColorBufferData [] = {
       1.0f, 1.0f, .0f,
       1.0f, 1.0f, .0f,
@@ -507,6 +653,7 @@ int main( void ) {
       uiNumAstVertices, World::CUBOID, 40000000*D, 0.8, vec3(-0.0, 0.0, 0.0),
       vec3(0.0f, 0.0f, 0.0f));
    */
+   //glEnable(GL_CULL_FACE);
    bool shot = false;
    int hitCount = 0;
    vec3 sizeOfBullet = vec3 (100, 100, 100);
--- playground/vertexshader.glsl
+++ playground/vertexshader.glsl
@@ -3,7 +3,6 @@
 //  Input vertex data, different for all executions of this shader.
 layout(location = 0) in vec3 vertexPosition_modelspace;
 //Tut4 not tut5
-layout(location = 1) in vec3 vertexColor;
 out vec3 fragmentColor;
 
 ////Tut5 not tut4
@@ -11,12 +10,17 @@ out vec3 fragmentColor;
 //  out vec2 UV;
 
 uniform mat4 MVP;
+uniform float zHalfMax;
 void main(){
     //Tut2
     gl_Position = MVP * vec4(vertexPosition_modelspace,1);
     //Tut4
-    fragmentColor=vertexColor;
-//      //Tut5
+    float color = 3*(vertexPosition_modelspace.z+zHalfMax)/(2*zHalfMax)-1;
+    /* fragmentColor.x=color>1?0:color; */
+    /* fragmentColor.y=color>2?0:(color>1?color-1:0); */
+    /* fragmentColor.z=color>2?color-2:0; */
+    fragmentColor = vec3(1.0,1.0,1.0);
+    //      //Tut5
 //      UV =  vertexUV;
 }
 
