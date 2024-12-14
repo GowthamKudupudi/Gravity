85a2909eaa6f93ab92214b11dfdf5d814c520800
Necktwi Ozfguah <Necktwi@FerryFair.com>
2024-12-15 02:45:35 +0530

    normalizedCodeGoodShadowsAndLight

--- playground/playgroundfragment.glsl
+++ playground/playgroundfragment.glsl
@@ -3,14 +3,39 @@
 //Tut4 not tut5
 in vec3 fColor;
 in vec4 shdwPos;
+in float dfusComp;
 out vec4 color;
 
 uniform sampler2D shadowMap;
+//uniform sampler2DShadow shadowMap;
 
 void main() {
    float visibility=1.0;
-   if (texture(shadowMap, shdwPos.xy).x <= shdwPos.z) {
-      visibility=0.5;
+   float bias = 0.005;
+   if (dfusComp>0.0) {
+      if (texture(shadowMap, shdwPos.xy).x<shdwPos.z-bias) {
+         visibility=0.5;
+      }
+      /* for (int i=0; i<4; i++) { */
+      /*    // use either : */
+      /*    //  - Always the same samples. */
+      /*    //    Gives a fixed pattern in the shadow, but no noise */
+      /*    int index = i; */
+      /*    //  - A random sample, based on the pixel's screen location.  */
+      /*    //    No banding, but the shadow moves with the camera, which looks weird. */
+      /*    // int index = int(16.0*random(gl_FragCoord.xyy, i))%16; */
+      /*    //  - A random sample, based on the pixel's position in world space. */
+      /*    //    The position is rounded to the millimeter to avoid too much aliasing */
+      /*    // int index = int(16.0*random(floor(Position_worldspace.xyz*1000.0), i))%16; */
+		
+      /*    // being fully in the shadow will eat up 4*0.2 = 0.8 */
+      /*    // 0.2 potentially remain, which is quite dark. */
+      /*    float shd = texture( */
+      /*       shadowMap, */
+      /*       vec3(shdwPos.xy,// + poissonDisk[index]/700.0, */
+      /*            (shdwPos.z-bias)/shdwPos.w)); */
+      /*    visibility -= 0.2*(1.0-shd); */
+      /* } */
    }
-   color = vec4(fColor, 1.0)*visibility;
+   color = vec4(fColor*dfusComp*visibility+0.1, 1.0);
 }
--- playground/playgroundgeom.glsl
+++ playground/playgroundgeom.glsl
@@ -7,18 +7,20 @@ in vec3 fragColor[];
 in vec4 shadowPos[];
 out vec3 fColor;
 out vec4 shdwPos;
+out float dfusComp;
 uniform mat4 VP;
 uniform float magnitude;
 
 vec3 normalOf (vec4 tri[3]) {
    vec3 a = tri[2].xyz - tri[0].xyz;
    vec3 b = tri[1].xyz - tri[0].xyz;
-   return normalize(cross(a, b));
+   //clockwise rotation is positive
+   return normalize(cross(b, a));
 }
 
 vec4 explode (vec4 position, vec3 normal, float magnitude) {
    vec3 direction = normal * magnitude;
-   return position - vec4(direction, 0);
+   return position + vec4(direction, 0);
 }
 
 // Default main function
@@ -28,22 +30,22 @@ void main() {
    tri[1] = (gl_in[1].gl_Position);
    tri[2] = (gl_in[2].gl_Position);
    vec3 normal = normalOf(tri);
-   vec3 liDir = vec3(-1.0, 0.0, 0.0);
-   float dfusComp = (0.5+dot(liDir,normal))/2;
-//   float dfusComp = dot(liDir,normal);
-   fColor = fragColor[0]*dfusComp;
+   vec3 liDir = vec3(1.0, 0.0, 0.0);
+   float dc = dot(liDir,normal);
+   fColor = fragColor[0];
    shdwPos = shadowPos[0];
+   dfusComp=dc;
 //   gl_Position = tri[0];
    gl_Position = VP*explode(tri[0], normal, magnitude);
    EmitVertex();
 
-   fColor = fragColor[1]*dfusComp;
+   fColor = fragColor[1];
    shdwPos = shadowPos[1];
 //   gl_Position = tri[1];
    gl_Position = VP*explode(tri[1], normal, magnitude);
    EmitVertex();
 
-   fColor = fragColor[2]*dfusComp;
+   fColor = fragColor[2];
    shdwPos = shadowPos[2];
 //   gl_Position = tri[2];
    gl_Position = VP*explode(tri[2], normal, magnitude);
